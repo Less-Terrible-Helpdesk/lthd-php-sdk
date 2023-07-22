fd5692f1d689c56d2e1b649fe6f93530b3a8a9a3
Krishnan <ks@geedesk.com>
2023-07-22 15:38:17 +0530

    Basic working sdk with http client

--- src/DaySupport.php
+++ src/DaySupport.php
@@ -9,96 +9,54 @@ class DaySupport
 	protected $api_key;
 	protected $client;
 	protected $auth_header;
+	protected $api_base_url;
+	protected $api_version;
 
-	public function __construct($api_key) 
+	public function __construct($api_base_url, $api_version, $api_key) 
 	{
+		$this->api_base_url = $api_base_url.'/'.$api_version;
 		$this->client = new Http();
 		$this->api_key = $api_key;
 		$this->auth_header = 'Authorization: Bearer '.$this->api_key;
 	}
-	
-	const API_BASE_URL = 'https://api.lessterriblehelpdesk.com';
 
-	public function create_ticket() 
+
+	public function create_ticket($project_id, $heading, $description, $priority, $customer_id, $contact_fname, $contact_lname, $contact_email) 
 	{
-		
+		$api_url = $this->api_base_url.'/tickets/create';
+		$data = [
+			'project_id' => $project_id, 
+			'heading' => $heading,
+			'description' => $description, 
+			'priority' => $priority, 
+			'customer_id' => $customer_id, 
+			'contact_fname' => $contact_fname, 
+			'contact_lname' => $contact_lname,
+			'contact_email' => $contact_email
+		];
+		$headers = array('Authorization' => $this->auth_header);
+		$response = $this->client->post($api_url, $data, $headers);
+		echo $response->getBody();
 	}
 
 
 	public function get_ticket($ticket_id) 
 	{
-		$api_url = DaySupport::API_BASE_URL.'/v1/tickets/'.$ticket_id;
+		$api_url = $this->api_base_url.'/tickets/'.$ticket_id;
 		$data = [];
-		$headers = array(
-			'Authorization' => $this->auth_header);
+		$headers = array('Authorization' => $this->auth_header);
 		$response = $this->client->get($api_url, $data, $headers);
 		echo $response->getBody();
 	}
 
 
-	public function take_ticket() 
+	public function get_customer_tickets($customer_id) 
 	{
-
-	}
-
-
-	public function change_ticket_priority() 
-	{
-
-	}
-
-
-	public function change_ticket_status() 
-	{
-
-	}
-
-
-	public function create_ticket_comment() 
-	{
-
-	}
-
-
-	public function get_ticket_comments() 
-	{
-
-	}
-
-
-	public function change_ticket_owner() 
-	{
-
-	}
-
-
-	public function merge_ticket() 
-	{
-
-	}
-
-
-	public function add_blocking_tickets() 
-	{
-
-	}
-
-
-	public function add_related_tickets() 
-	{
-
-	}
-
-
-	public function get_project_tickets() 
-	{
-
-	}
-
-
-	public function get_customer_tickets() 
-	{
-
+		$api_url = $this->api_base_url.'/customers/tickets/'.$customer_id.'?status=all&priority=all';
+		$data = [];
+		$headers = array('Authorization' => $this->auth_header);
+		$response = $this->client->get($api_url, $data, $headers);
+		echo $response->getBody();
 	}
 
 
