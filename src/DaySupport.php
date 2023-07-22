627ef25b679aa5d9b432e1e4276c3412f1021594
Krishnan <ks@geedesk.com>
2023-07-22 10:18:58 +0530

    Basic Daysupport client sdk scaffolding

---
+++ src/DaySupport.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace DaySupport\PhpSdk;
+
+
+class DaySupport 
+{
+
+	public function user_auth() 
+	{
+		
+	}
+
+
+	public function create_ticket() 
+	{
+
+	}
+
+
+	public function get_ticket() 
+	{
+
+	}
+
+
+	public function take_ticket() 
+	{
+
+	}
+
+
+	public function change_ticket_priority() 
+	{
+
+	}
+
+
+	public function change_ticket_status() 
+	{
+
+	}
+
+
+	public function create_ticket_comment() 
+	{
+
+	}
+
+
+	public function get_ticket_comments() 
+	{
+
+	}
+
+
+	public function change_ticket_owner() 
+	{
+
+	}
+
+
+	public function merge_ticket() 
+	{
+
+	}
+
+
+	public function add_blocking_tickets() 
+	{
+
+	}
+
+
+	public function add_related_tickets() 
+	{
+
+	}
+
+
+	public function get_project_tickets() 
+	{
+
+	}
+
+
+	public function get_customer_tickets() 
+	{
+
+	}
+
+
+}
